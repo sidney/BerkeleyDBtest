076677767b0a089d06d2031b156385609e548707
Sidney Markowitz <sidney@sidney.com>
2024-05-03 10:28:26 +1200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # BerkeleyDBtest
 Self contained test to demonstrate BerkeleyDB bug on FreeBSD arm64
+
+Build on a system that has BerkelyDB 18 libraries installed.
+
+## Building
+```text
+autoreconf --install
+./configure
+make
+```
+
+Running the bdbtest executable the first time creates BerkeleyDB
+shared memory region files for an environment. Running it a second
+time should open those files for reuse. Instead on FreeBSD arm64 it
+gets an error trying to lock a pthread mutex.
---
+++ src/bdbtest.c
@@ -0,0 +1,47 @@
+#include <db.h>
+#include <stdio.h>
+
+int main() {
+    DB_ENV *env;
+    u_int32_t env_flags = DB_CREATE | DB_INIT_MPOOL;
+    // Create a BerkeleyDB environment
+    printf("Creating BerkeleyDB environment...\n");
+    int ret = db_env_create(&env, 0);
+    if (ret != 0) {
+        // Handle error
+        printf("Error creating BerkeleyDB environment: %s\n", db_strerror(ret));
+        return ret;
+    }
+
+    // Set the database home directory
+    printf("Setting database home directory...\n");
+    ret = env->set_data_dir(env, "/Users/sidney/tmp/bdbtest/user_state");
+    if (ret != 0) {
+        // Handle error
+        printf("Error setting database home directory: %s\n", db_strerror(ret));
+        env->close(env, 0);
+        return ret;
+    }
+
+    // Open the BerkeleyDB environment
+    printf("Opening BerkeleyDB environment...\n");
+    ret = env->open(env, NULL, env_flags, 0);
+    if (ret != 0) {
+        // Handle error
+        printf("Error opening BerkeleyDB environment: %s\n", db_strerror(ret));
+        env->close(env, 0);
+        return ret;
+    }
+
+    // Close the BerkeleyDB environment
+    /*printf("Closing BerkeleyDB environment...\n");
+    ret = env->close(env, 0);
+    if (ret != 0) {
+        // Handle error
+        printf("Error closing BerkeleyDB environment: %s\n", db_strerror(ret));
+        return ret;
+    }*/
+    printf("BerkeleyDB environment closed.\n");
+    return 0;
+}
+
